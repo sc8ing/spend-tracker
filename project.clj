6f5e95b4cb1b10d7fdf1ff225bc40e0df9a17486
Jacob Bennett <16541352+sc8ing@users.noreply.github.com>
2023-10-12 15:13:28 -0400

    add htmx scaffolding and basic setup

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://fatobesegoo.se"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring/ring-core "1.10.0"]
-                 [ring/ring-jetty-adapter "1.10.0"]]
+                 [ring/ring-jetty-adapter "1.10.0"]
+                 [hiccup "2.0.0-RC2"]
+                 [compojure "1.7.0"]]
   :main ^:skip-aot spend-tracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/spend_tracker/core.clj
+++ src/spend_tracker/core.clj
@@ -1,7 +1,61 @@
 (ns spend-tracker.core
+  (:require [clojure.pprint :refer [pprint]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [hiccup2.core :as h]
+            [compojure.core :as compojure]
+            [compojure.route :as route])
   (:gen-class))
 
+(defn in-page [content &{:keys [title] :or {title "Spend Tracker"}}]
+  (h/html
+    [:head
+      [:title "Spend Tracker"]
+      [:link {:rel "stylesheet" :href "/css/style.css"}]
+      [:script {:src "/js/htmx.min.js"}]]
+    [:body
+     [:h1 title]
+     content]))
+
+(def record-txn-input
+  (h/html
+   [:div [:form {:method "post"
+                 :action "/transaction"
+                 :hx-post "/transaction"}
+          [:input {:type "number"
+                   :name "amount"}]
+          [:input {:type "text"
+                   :name "category-and-tags"}]
+          [:input {:type "submit"
+                   :value "Add"}]]]))
+
+(defn root-page [request]
+  (println "request" (pprint request))
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (-> record-txn-input
+             (in-page {:title "Record Transaction"})
+             str)})
+
+(defn record-transaction [{:keys [amount category-and-tags]}]
+  (println "recording transaction: " amount category-and-tags)
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str record-txn-input)})
+
+(compojure/defroutes app-routes
+  (compojure/GET "/" _ root-page)
+  (compojure/POST "/transaction" {params :params} (record-transaction params))
+  (route/not-found "Not Found"))
+
+(def app (-> #'app-routes
+             (wrap-keyword-params)
+             (wrap-params)
+             (wrap-resource "public")))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println x))
+  [& _]
+  (run-jetty #'app {:port 3000
+                    :join? false}))
